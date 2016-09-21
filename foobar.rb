47d3750048443ff07a990070bafe20e17785ee17
Franklin <heng.franklin@gmail.com>
2016-09-21 01:06:37 -0700

    finished hw

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,16 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    a.map! do |e| 
+    	e.to_i
+    end 
+    a.map! do |e| 
+    	e+2
+    end
+    a.delete_if {|n| n%2==1}
+    a = a.uniq
+    a = a.delete_if {|n| n > 10}
+    return a.inject(:+)
   end
 end
 
